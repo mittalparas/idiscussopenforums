b7a754f6149d5a08cb8e7def43c0a709eab1d245
mittalparas <200574189@student.georgianc.on.ca>
2023-11-20 23:30:54 -0500

    Adding footer in the HTML

--- index.php
+++ index.php
@@ -81,3 +81,10 @@ require 'partials/_dbconnect.php';
 
     </div>
   </div>
+
+  <!-- this is footer  -->
+  <?php require 'partials/_footer.php' ?>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</body>
+
+</html>
