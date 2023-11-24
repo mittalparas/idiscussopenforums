4feca85f43caa6a6b2ac359a5a009c0c04fe2dc8
mittalparas <200574189@student.georgianc.on.ca>
2023-11-23 20:42:13 -0500

    Modifing about.php page

--- about.php
+++ about.php
@@ -5,3 +5,12 @@ require 'partials/_dbconnect.php';
 
 <!doctype html>
 <html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>iDiscuss - about</title>
+     <link rel="icon" href="images/logo.jpg" type="image/x-icon">
+  <link rel="shortcut icon" href="images/logo.jpg" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
