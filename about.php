9b4de75659b4ff5fa0f28008e40ff552a74be09d
mittalparas <200574189@student.georgianc.on.ca>
2023-11-25 00:34:45 -0500

    Adding about.php file

--- about.php
+++ about.php
@@ -14,3 +14,8 @@ require 'partials/_dbconnect.php';
   <link rel="shortcut icon" href="images/logo.jpg" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
+
+<body>
+
+    <!-- Navbar is in the header -->
+    <?php require 'partials/_header.php' ?>
