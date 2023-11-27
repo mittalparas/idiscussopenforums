46d71d9198e727f18e00f04b5ecfdf05b30e8876
mittalparas <200574189@student.georgianc.on.ca>
2023-11-26 21:11:55 -0500

    this is a commit

--- about.php
+++ about.php
@@ -19,3 +19,12 @@ require 'partials/_dbconnect.php';
 
     <!-- Navbar is in the header -->
     <?php require 'partials/_header.php' ?>
+    <div class="container" style="min-height: 800px">
+        <div class="container-fluid py-5">
+            <h2 class="fw-bold">Made with <img src="images/heart.jpeg" height="64px" width="64px"> <span class="text-danger">Love</span> by <span class="text-success">Paras Mittal</span></h2>
+            <h2 class="text-center my-5" style="font-family: Roboto"><i>Special thanks to</i></h2>
+            <div class="p-5 mb-4 bg-body-tertiary rounded-3 bg-danger-subtle">
+                <div class="container-fluid py-5">
+                    <h1 class="display-5 fw-bold">code with harry</h1>
+                    <p class="col-md-8 fs-4">Best free and online learning platform for programmers.</p>
+                    <a href="https://www.youtube.com/playlist?list=PLu0W_9lII9aikXkRE0WxDt1vozo3hnmtR" target="_blank"><button class="btn btn-danger btn-lg" type="button"><img src="images/youtubeIcon.jpeg" height="20px" width="40px"> YouTube</button></a>
